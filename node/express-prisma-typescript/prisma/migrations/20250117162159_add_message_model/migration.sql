43d5eb6b589f3b23c34e9898adf5e94970427dad
Federico Rossendy <fede070893@gmail.com>
2025-01-23 12:50:15 -0300

    Add database migrations

---
+++ node/express-prisma-typescript/prisma/migrations/20250117162159_add_message_model/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "text" TEXT NOT NULL,
+    "senderId" UUID NOT NULL,
+    "recipientId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_recipientId_fkey" FOREIGN KEY ("recipientId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
